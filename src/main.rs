73e743def0e0659aad7420a1ffad0c67a7b1b69d
Aaron Eline <aeline@amazon.com>
2023-04-19 14:38:23 +0000

    Test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
     println!("Hello, world!");
 }
+
+
+fn add_numbers(x : i32, y : i32) -> i32 {
+    x + y + 2
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn simple() {
+        assert_eq!(add_numbers(1, 2), 3);
+    }
+}
