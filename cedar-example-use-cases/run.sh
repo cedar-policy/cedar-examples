e306850a1e8a3907a9e5cd2ac80989bf953b7b77
Shaobo He <shaobohe@amazon.com>
2023-05-08 14:45:34 -0700

    shellcheck

--- cedar-example-use-cases/run.sh
+++ cedar-example-use-cases/run.sh
@@ -2,33 +2,34 @@
 
 failed() {
     message=$1
-    echo $message
+    echo "$message"
     exit 1
 }
 
 validate() {
     local folder=$1
     echo "Running validation on ${folder}"
-    cd $folder
-    res=$(cedar validate --policies policies.cedar --schema schema.json)
-    if [[ $res != "Validation Passed" ]]
-    then
-        failed "Failed validation on ${folder} with result: ${res}"
-    else
-        echo "Validation succeeded"
-    fi
-    cd ..
+    (
+     cd "$folder" || exit
+     res=$(cedar validate --policies policies.cedar --schema schema.json)
+     if [[ $res != "Validation Passed" ]]
+     then
+         failed "Failed validation on ${folder} with result: ${res}"
+     else
+         echo "Validation succeeded"
+     fi
+    )
 }
 
 authorize() {
     local folder=$1
     echo "Running authorization on ${folder}"
     verdict=$2
-    cd $folder
+    cd "$folder" || exit
     for file in *.json
     do
-        res=$(cedar authorize --policies ../policies.cedar --entities ../entities.json --request-json $file)
-        if [ $res != $verdict ]
+        res=$(cedar authorize --policies ../policies.cedar --entities ../entities.json --request-json "$file" | xargs)
+        if [ "$res" != "$verdict" ]
         then
             failed "Failed authorization on ${file} with result: ${res}"
         else
@@ -39,11 +40,11 @@ authorize() {
 }
 
 folders=("document_cloud" "github_example")
-for folder in ${folders[@]};
+for folder in "${folders[@]}";
 do
     #validate policies
-    validate $folder
+    validate "$folder"
     # authorize policies
-    authorize $folder"/allow_requests" ALLOW
-    authorize $folder"/deny_requests" "DENY"
+    authorize "$folder""/allow_requests" ALLOW
+    authorize "$folder""/deny_requests" "DENY"
 done
