4d99b652f82c62f75e777a35db5cbea854bc88a1
Andrew Wells <anmwells@amazon.com>
2023-05-09 16:40:43 -0700

    fix cedar-java location

--- cedar-java-hello-world/config.sh
+++ cedar-java-hello-world/config.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-grand_parent_dir="$(dirname $(dirname $(pwd)))"
+parent_dir="$(dirname $(pwd))"
 
 #Try to set correctly for Mac and Linux machines
 if [ "$(uname)" == "Darwin" ]; then
-ffi_lib_str="    environment 'CEDAR_JAVA_FFI_LIB', '"$grand_parent_dir"/cedar-java/CedarJavaFFI/target/debug/libcedar_java_ffi.dylib'"
+ffi_lib_str="    environment 'CEDAR_JAVA_FFI_LIB', '"$parent_dir"/cedar-java/CedarJavaFFI/target/debug/libcedar_java_ffi.dylib'"
 else
-ffi_lib_str="    environment 'CEDAR_JAVA_FFI_LIB', '"$grand_parent_dir"/cedar-java/CedarJavaFFI/target/debug/libcedar_java_ffi.so'"
+ffi_lib_str="    environment 'CEDAR_JAVA_FFI_LIB', '"$parent_dir"/cedar-java/CedarJavaFFI/target/debug/libcedar_java_ffi.so'"
 fi
 
 sed "85s;.*;$ffi_lib_str;" "build.gradle" > new_build.gradle
