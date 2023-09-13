71e130ac6af8999bcef4cd1bd02bfbed6a83d844
Andrew Wells <130512013+andrewmwells-amazon@users.noreply.github.com>
2023-09-13 13:48:38 -0700

    update build to use maven central (#54)

--- cedar-java-hello-world/config.sh
+++ cedar-java-hello-world/config.sh
@@ -9,5 +9,5 @@ else
 ffi_lib_str="    environment 'CEDAR_JAVA_FFI_LIB', '"$curr_dir"/cedar-java/CedarJavaFFI/target/debug/libcedar_java_ffi.so'"
 fi
 
-sed "85s;.*;$ffi_lib_str;" "build.gradle" > new_build.gradle
+sed "90s;.*;$ffi_lib_str;" "build.gradle" > new_build.gradle
 mv new_build.gradle build.gradle
