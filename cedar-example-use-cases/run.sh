1e1d23abf3740ff1eb423f0ffb0c4e4775576e2e
Shaobo He <shaobohe@amazon.com>
2024-03-14 10:56:29 -0700

    document_cloud and github
    Signed-off-by: Shaobo He <shaobohe@amazon.com>

--- cedar-example-use-cases/run.sh
+++ cedar-example-use-cases/run.sh
@@ -4,12 +4,12 @@ source ../test_utils.sh
 
 # PhotoApp
 echo -e "\nTesting github_example..."
-validate "github_example" "policies.cedar" "github_example.cedarschema.json"
+validate "github_example" "policies.cedar" "github_example.cedarschema"
 authorize "github_example" "policies.cedar" "entities.json"
 
 # GitApp
 echo -e "\nTesting document_cloud..."
-validate "document_cloud" "policies.cedar" "document_cloud.cedarschema.json"
+validate "document_cloud" "policies.cedar" "document_cloud.cedarschema"
 authorize "document_cloud" "policies.cedar" "entities.json"
 
 exit "$any_failed"
