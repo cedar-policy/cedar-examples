e18b9e695e2681858ec5cc36fddc1fd866f17365
Mike Hicks <mwhicks@amazon.com>
2024-05-21 17:21:22 -0400

    new sample app; update test infra

--- cedar-example-use-cases/run.sh
+++ cedar-example-use-cases/run.sh
@@ -14,4 +14,10 @@ validate "document_cloud" "policies.cedar" "document_cloud.cedarschema"
 authorize "document_cloud" "policies.cedar" "entities.json"
 format "document_cloud" "policies.cedar"
 
+# FIS
+echo -e "\nTesting Tags & Roles..."
+validate "tags_n_roles" "policies.cedar" "policies.cedarschema"
+authorize "tags_n_roles" "policies.cedar" "entities.json" "policies.cedarschema"
+format "tags_n_roles" "policies.cedar"
+
 exit "$any_failed"
