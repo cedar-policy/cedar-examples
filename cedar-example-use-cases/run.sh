64590658f569f1db8006c589572687677c122d1c
Kesha Hietala <khieta@amazon.com>
2024-05-21 15:15:21 -0400

    add format check

--- cedar-example-use-cases/run.sh
+++ cedar-example-use-cases/run.sh
@@ -6,10 +6,12 @@ source ../test_utils.sh
 echo -e "\nTesting github_example..."
 validate "github_example" "policies.cedar" "github_example.cedarschema"
 authorize "github_example" "policies.cedar" "entities.json"
+format "github_example" "policies.cedar"
 
 # GitApp
 echo -e "\nTesting document_cloud..."
 validate "document_cloud" "policies.cedar" "document_cloud.cedarschema"
 authorize "document_cloud" "policies.cedar" "entities.json"
+format "document_cloud" "policies.cedar"
 
 exit "$any_failed"
