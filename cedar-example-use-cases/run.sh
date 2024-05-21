f3faec8f654b5eac44acdca07fa0f2f1eae7d6a6
Michael Hicks <mwhicks@amazon.com>
2024-05-21 18:34:11 -0400

    Update run.sh
    Comment typos

--- cedar-example-use-cases/run.sh
+++ cedar-example-use-cases/run.sh
@@ -2,19 +2,19 @@
 
 source ../test_utils.sh
 
-# PhotoApp
+# GitHub
 echo -e "\nTesting github_example..."
 validate "github_example" "policies.cedar" "github_example.cedarschema"
 authorize "github_example" "policies.cedar" "entities.json"
 format "github_example" "policies.cedar"
 
-# GitApp
+# Doc Cloud
 echo -e "\nTesting document_cloud..."
 validate "document_cloud" "policies.cedar" "document_cloud.cedarschema"
 authorize "document_cloud" "policies.cedar" "entities.json"
 format "document_cloud" "policies.cedar"
 
-# FIS
+# Tags & roles
 echo -e "\nTesting Tags & Roles..."
 validate "tags_n_roles" "policies.cedar" "policies.cedarschema"
 authorize "tags_n_roles" "policies.cedar" "entities.json" "policies.cedarschema"
