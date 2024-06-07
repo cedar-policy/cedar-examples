ef82889b0a81f10278f3fd788782aff999aa1ed6
Michael Hicks <mwhicks@amazon.com>
2024-06-07 14:01:04 -0400

    Tax preparer example (#151)
    Signed-off-by: Mike Hicks <mwhicks@amazon.com>

--- cedar-example-use-cases/run.sh
+++ cedar-example-use-cases/run.sh
@@ -20,6 +20,12 @@ validate "tags_n_roles" "policies.cedar" "policies.cedarschema"
 authorize "tags_n_roles" "policies.cedar" "entities.json" "policies.cedarschema"
 format "tags_n_roles" "policies.cedar"
 
+# Tax preparer
+echo -e "\nTesting Tax preparer..."
+validate "tax_preprarer" "policies.cedar" "policies.cedarschema" "linked"
+authorize "tax_preprarer" "policies.cedar" "entities.json" "policies.cedarschema" "linked"
+format "tax_preprarer" "policies.cedar"
+
 # Sales org static
 echo -e "\nTesting Sales Orgs (static)..."
 validate "sales_orgs/static" "policies.cedar" "policies.cedarschema"
@@ -32,13 +38,13 @@ validate "sales_orgs/templated" "policies.cedar" "policies.cedarschema" "linked"
 authorize "sales_orgs/templated" "policies.cedar" "entities.json" "policies.cedarschema" "linked"
 #format "sales_orgs/templated" "policies.cedar"
 
-# Hotel chains
+# Hotel chains static
 echo -e "\nTesting Hotels (static)..."
 validate "hotel_chains/static" "policies.cedar" "policies.cedarschema"
 authorize "hotel_chains/static" "policies.cedar" "entities.json" "policies.cedarschema"
 #format "hotel_chains/static" "policies.cedar"
 
-# Hotel chains
+# Hotel chains templated
 echo -e "\nTesting Hotels (templated)..."
 validate "hotel_chains/templated" "policies.cedar" "policies.cedarschema" "linked"
 authorize "hotel_chains/templated" "policies.cedar" "entities.json" "policies.cedarschema" "linked"
