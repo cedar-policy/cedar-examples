7a6aafe1c80547715f100ec27b3ee4d35f0eb4fd
shaobo-he-aws <130499339+shaobo-he-aws@users.noreply.github.com>
2024-06-27 11:11:42 -0700

    Revert "Updates for cedar-policy/cedar#1026 (#164)" (#166)
    Signed-off-by: Shaobo He <shaobohe@amazon.com>

--- tinytodo/test_tinytodo.py
+++ tinytodo/test_tinytodo.py
@@ -69,9 +69,8 @@ class TinyTodoTest(unittest.TestCase):
         set_user(emina)
         self.assert_in_stdout("Created task", lambda : create_task(0, "bar"))
         self.assert_in_stdout("1: [ ] bar", lambda : get_list(0))
-        
+
     def test_get_lists(self):
         self.assert_in_stdout("Created list ID 0", lambda : create_list("foo"))
         self.assert_in_stdout("Created list ID", lambda : create_list("bar"))
         self.assert_in_stdout('Lists: foo,bar', lambda: get_lists())
-        
