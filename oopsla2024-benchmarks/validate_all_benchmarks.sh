0f7ba42bff28f0582a7c1d1f6df29c9b5b179028
Kesha Hietala <khieta@amazon.com>
2024-09-23 14:15:33 -0600

    add --schema-format json

--- oopsla2024-benchmarks/validate_all_benchmarks.sh
+++ oopsla2024-benchmarks/validate_all_benchmarks.sh
@@ -1,16 +1,16 @@
 BENCHES="${BENCHES:-benches}"
 
 # gdrive benchmark (static policies)
-cedar validate --policies $BENCHES/gdrive/cedar/policies.cedar --schema $BENCHES/gdrive/cedar/gdrive.cedarschema.json
+cedar validate --policies $BENCHES/gdrive/cedar/policies.cedar --schema $BENCHES/gdrive/cedar/gdrive.cedarschema.json --schema-format json
 
 # gdrive benchmark (templates)
-cedar validate --policies $BENCHES/gdrive-templates/cedar/policies.cedar --schema $BENCHES/gdrive-templates/cedar/gdrive-templates.cedarschema.json
+cedar validate --policies $BENCHES/gdrive-templates/cedar/policies.cedar --schema $BENCHES/gdrive-templates/cedar/gdrive-templates.cedarschema.json --schema-format json
 
 # gdrive benchmark (static policies)
-cedar validate --policies $BENCHES/github/cedar/policies.cedar --schema $BENCHES/github/cedar/github.cedarschema.json
+cedar validate --policies $BENCHES/github/cedar/policies.cedar --schema $BENCHES/github/cedar/github.cedarschema.json --schema-format json
 
 # gdrive benchmark (templates)
-cedar validate --policies $BENCHES/github-templates/cedar/policies.cedar --schema $BENCHES/github-templates/cedar/github-templates.cedarschema.json
+cedar validate --policies $BENCHES/github-templates/cedar/policies.cedar --schema $BENCHES/github-templates/cedar/github-templates.cedarschema.json --schema-format json
 
 # tinytodo benchmark
-cedar validate --policies $BENCHES/tinytodo/cedar/tinytodo.cedar --schema $BENCHES/tinytodo/cedar/tinytodo.cedarschema.json
+cedar validate --policies $BENCHES/tinytodo/cedar/tinytodo.cedar --schema $BENCHES/tinytodo/cedar/tinytodo.cedarschema.json --schema-format json
